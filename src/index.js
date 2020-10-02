22a8aa941711e6871a2637448c3d8b28291b5919
all-home <mrp99999999@gmail.com>
2020-10-02 17:11:21 +0300

    feat: add algoritmics

--- src/index.js
+++ src/index.js
@@ -1,12 +1,44 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array.length == 0 )
+  {
+    return 0
+  }
+  let res = array[0];
+  for(let i= 0; i < array.length; i++)
+  {
+    if ((array[i]) < res)
+    {
+      res = array[i];
+    }
+
+  }
+  
+  return res;
 }
 
 exports.max = function max (array) {
-  return 0;
+  
+  let res = array[0];
+  for(let i= 0; i < array.length; i++)
+  {
+    if ((array[i]) > res)
+    {
+      res = array[i];
+    }
+
+  }
+  
+  return res;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  let res = 0;
+
+  for(let i= 0; i < array.length; i++)
+  {
+    res += array[i]; 
+
+  }
+  return res/array.length;
 }
